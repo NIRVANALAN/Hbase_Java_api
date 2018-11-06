c7903b7d3618e7f9da698a0994658e977771fdb1
maver <maverickers@outlook.com>
2018-11-06 03:56:25 -0800

    connect well

--- src/main/java/com/hbase/test/HbaseCase.java
+++ src/main/java/com/hbase/test/HbaseCase.java
@@ -2,8 +2,12 @@ package com.hbase.test;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.util.Bytes;
+
 import java.io.IOException;
 
 /**
@@ -13,41 +17,53 @@ public class HbaseCase {
     public static Connection connection;
     public static Admin admin;
     public static Configuration configuration;
-    public static void main(String[] args) {
-        System.out.println("connect begin");
 
+    public static void main(String[] args) throws IOException{
 
-        System.out.println("connect ok");
-        try {
-            HTable table = new HTable(configuration, "tempTable");
-            table.setAutoFlush(false);
-            Put put = new Put(Bytes.toBytes("hello12"));
-            put.addColumn(Bytes.toBytes("cf"), Bytes.toBytes("cf"), Bytes.toBytes("vale"));
-            table.put(put);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        init();
+        createTable("tempTable",null);
+        close();
     }
-    public static void init(){
+
+    public static void init() {
+        System.out.println("connect begin");
         configuration = HBaseConfiguration.create();
-        configuration.set("hbase.rootdir","file:///usr/local/hbase/hbase-tmp");
-        try{
+        configuration.set("hbase.rootdir", "file:///usr/local/hbase/hbase-tmp");
+        try {
             connection = ConnectionFactory.createConnection(configuration);
             admin = connection.getAdmin();
+            System.out.println("connect ok");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    public static void close(){
+
+    public static void close() {
         try {
-            if (admin!=null)
+            if (admin != null)
                 admin.close();
-            if (null!=connection)
+            if (null != connection)
                 connection.close();
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
+
+    public static void createTable(String myTableName, String[] colFamily) throws IOException {
+        init();
+        TableName tableName = TableName.valueOf(myTableName);
+        if (admin.tableExists(tableName)) {
+            System.out.println("table exist");
+        } else {
+            HTableDescriptor hTableDescriptor = new HTableDescriptor(tableName);
+            for (String str : colFamily) {
+                HColumnDescriptor hColumnDescriptor = new HColumnDescriptor(str);
+                hTableDescriptor.addFamily(hColumnDescriptor);
+            }
+            admin.createTable(hTableDescriptor);
+            ;
+        }
+        close();
     }
 }
